03e4862de90289afbb29fdc64f4a1e53e8d65167
sanaanymoe <sanaany1992@gmail.com>
2020-07-05 13:35:22 -0700

    add backend routes and validations for tweets

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,6 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport')
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
+const tweets = require("../../validation/tweets");
+const { findById } = require("../../models/Tweet");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the tweets route" }));
+
+router.get("/", (req, res) => {
+    Tweet
+        .find()
+        .sort({ data: -1 })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(400).json(err));
+})
+
+router.get("/user/:user_id", (req, res) => {
+    Tweet
+        .find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(400).json(err));
+})
+
+router.get("/:id", (req, res) => {
+    Tweet
+        .findById(req.params.id)
+        .then((tweet) => res.json(tweet))
+        .catch((err) => res.status(400).json(err));
+})
+
+router.post("/",
+    passport.authenticate("jwt", {session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateTweetInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors)
+        }
+
+        const newTweet = new Tweet({
+            user: req.user.id,
+            text: req.body.text
+        });
+
+        newTweet
+            .save()
+            .then(tweet => res.json(tweet));
+    }
+)
 
 module.exports = router;
